2bdb930ed1db5bbdb56492a8fb128219800485d9
Rafciooo <rafix_007@interia.eu>
2020-02-15 12:19:55 +0100

    Konfiguracja TwojLekarz

---
+++ src/main/java/Lekarz/TwojLekarz/TwojLekarzApplication.java
@@ -0,0 +1,13 @@
+package Lekarz.TwojLekarz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojLekarzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwojLekarzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lekarz/TwojLekarz/TwojLekarzApplicationTests.java
@@ -0,0 +1,13 @@
+package Lekarz.TwojLekarz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojLekarzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
